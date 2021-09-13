368645425703339220ebb1154fa26a15989b1432
Maxime Welcklen <maxime.welcklen@he-arc.ch>
2021-09-13 16:43:04 +0200

    add HTTPS remote url

--- src/api/ApiCaller.js
+++ src/api/ApiCaller.js
@@ -1,6 +1,6 @@
 
 const localUrl = "http://localhost:8000/api";
-const remoteUrl = "http://ec2-35-180-118-37.eu-west-3.compute.amazonaws.com:8000/api";
+const remoteUrl = "https://www.noel-phoenix-backend.tk/api";
 const apiUrl = (process.env.NODE_ENV === 'production') ? remoteUrl : localUrl;
 
 
